fd731b211009633838dfd507c4af27dd0a533029
danilopereira <dansilva13@gmail.com>
2017-08-15 22:35:35 -0300

    pointed to my backend on heroku

--- js/services/LocationServices.js
+++ js/services/LocationServices.js
@@ -1,7 +1,7 @@
 app.factory('locationServices', ['$http', function($http) {
 
         function getLocationAPI(callback) {
-            return "https://notepadsaas.herokuapp.com";
+            return "https://notepsaas.herokuapp.com";
         }
 
         return {
